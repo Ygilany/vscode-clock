91370cea73da6c143696bad707f9a6896e61c80a
Yahya Gilany <yahya.gilany@live.com>
2017-04-01 23:49:04 -0400

    working extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,21 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getDateTime } from './timeFormatted';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "vscode-clock" is now active!');
+	const statusbar: vscode.StatusBarItem = vscode.window
+		.createStatusBarItem(vscode.StatusBarAlignment.Right, -1000); // higher number means more to the left
+	
+	statusbar.show();
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-clock" is now active!');
+	context.subscriptions.push(statusbar);
+	updateTime();
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
-}
-
-// this method is called when your extension is deactivated
-export function deactivate() {
+	function updateTime() {
+		setInterval(()=> {
+			statusbar.text = getDateTime();
+			console.log(statusbar.text)
+		}, 1000);
+	}
 }
