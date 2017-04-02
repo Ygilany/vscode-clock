1dfdd80d5ff15e1a27b888eedd4171c48cf9ff1c
Yahya Gilany <yahya.gilany@live.com>
2017-04-01 23:50:18 -0400

    remove log

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@ import { getDateTime } from './timeFormatted';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "vscode-clock" is now active!');
 	const statusbar: vscode.StatusBarItem = vscode.window
 		.createStatusBarItem(vscode.StatusBarAlignment.Right, -1000); // higher number means more to the left
 	
